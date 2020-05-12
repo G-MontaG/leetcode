e8011f6c4b7cac1552bbba57499e2bc8530d55aa
Artur Osypenko <artur.osypenko@dev-pro.net>
2020-05-12 22:39:57 +0300

    Add fibonacci and improve gitignore

---
+++ fibonacci/fibonacci.js
@@ -0,0 +1,16 @@
+const cache = {};
+
+/**
+ * @param {number} N
+ * @return {number}
+ */
+const climbStairs = (N) =>  {
+    if (N <= 1) {
+        return N;
+    }
+    if (cache[N] !== undefined) {
+        return cache[N]
+    } else {
+        return cache[N] = climbStairs(N - 1) + climbStairs(N - 2)
+    }
+};
---
+++ fibonacci/fibonacci.py
@@ -0,0 +1,12 @@
+class Solution:
+    def __init__(self):
+        self.cache = {}
+        
+    def climbStairs(self, N: int) -> int:
+        if N <= 1:
+            return N
+        if N in self.cache:
+            return self.cache[N]
+        else:
+            self.cache[N] = self.climbStairs(N - 1) + self.climbStairs(N - 2)
+            return self.cache[N]
