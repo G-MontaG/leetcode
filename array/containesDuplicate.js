c0cd16660c6c9786cd88b12ff2c3f32c058c31fc
Artur Osypenko <artur.osypenko@dev-pro.net>
2020-05-19 19:11:24 +0300

    Containe Duplicates 2 solution

--- array/containesDuplicate.js
+++ array/containesDuplicate.js
@@ -6,17 +6,3 @@
 const containsDuplicate = (nums) => {
     return new Set(nums).size !== nums.length;
 }
-
-// var containsDuplicate = function(nums) {
-//     let isDuplicates = false;
-//     const numsObj = nums.reduce((acc, value) => {
-//         if (!acc[value]) {
-//             acc[value] = 1;
-//         } else {
-//             acc[value]++;
-//             isDuplicates = true;
-//         }
-//         return acc;
-//     }, {})
-//     return isDuplicates;
-// };
---
+++ array/containesDuplicate2.py
@@ -0,0 +1,9 @@
+# https://leetcode.com/problems/contains-duplicate-ii
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        dic = {}
+        for i, v in enumerate(nums):
+            if v in dic and i - dic[v] <= k:
+                return True
+            dic[v] = i
+        return False
