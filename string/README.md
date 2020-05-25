e44e766e8ba2c38e0ad6919dc29d1cbd3b7ec5cf
Artur Osypenko <artur.osypenko@dev-pro.net>
2020-05-25 22:14:22 +0300

    Reverse string in-place solution

---
+++ string/README.md
@@ -0,0 +1,5 @@
+# String tricks
+
+### Reverse String
+
+Two pointer solution. Pointers moving toward each other until reach the middle of the string.
---
+++ string/reverseString.js
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/reverse-string/
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+const reverseString = (s) => {
+  let start = 0;
+  let end = s.length - 1;
+  while (start < end) {
+    [s[start], s[end]] = [s[end], s[start]];
+    start++;
+    end--;
+  }
+};
