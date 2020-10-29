080907d8b92e24e99c21377286ef73813d8cbc41
Artur Osypenko <artur.osypenko@dev-pro.net>
2020-10-29 13:53:27 +0200

    Upgrade backtraking code solutions

--- backtracking/README.md
+++ backtracking/README.md
@@ -2,4 +2,8 @@
 
 ## Letter Combinations of a Phone Number
 
+[Youtube](https://www.youtube.com/watch?v=a-sMgZ7HGW0)
+
 ## Generate Parentheses
+
+[Youtube](https://www.youtube.com/watch?v=sz1qaKt0KGQ)
--- backtracking/letterCombinationsOfAPhoneNumber.js
+++ backtracking/letterCombinationsOfAPhoneNumber.js
@@ -1,4 +1,26 @@
 // https://leetcode.com/problems/letter-combinations-of-a-phone-number/
+const map = {
+  2: "abc",
+  3: "def",
+  4: "ghi",
+  5: "jkl",
+  6: "mno",
+  7: "pqrs",
+  8: "tuv",
+  9: "wxyz",
+};
+
+const go = (index, path, digits, res) => {
+  if (index === digits.length) {
+    res.push(path);
+    return;
+  }
+
+  for (const c of map[digits[index]]) {
+    go(index + 1, path + c, digits, res);
+  }
+};
+
 /**
  * @param {string} digits
  * @return {string[]}
@@ -6,29 +28,7 @@
 const letterCombinations = (digits) => {
   if (digits == null || digits.length === 0) return [];
 
-  const map = {
-    2: "abc",
-    3: "def",
-    4: "ghi",
-    5: "jkl",
-    6: "mno",
-    7: "pqrs",
-    8: "tuv",
-    9: "wxyz",
-  };
-
   const res = [];
-  const go = (i, s) => {
-    if (i === digits.length) {
-      res.push(s);
-      return;
-    }
-
-    for (const c of map[digits[i]]) {
-      go(i + 1, s + c);
-    }
-  };
-
-  go(0, "");
+  go(0, "", digits, res);
   return res;
 };
